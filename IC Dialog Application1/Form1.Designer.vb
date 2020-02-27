526c530ee11da9cf3e5865fbb7e223b3948052ba
Graham <Michael.Graham@vadoc.virginia.gov>
2020-02-27 13:33:03 -0500

    Add project files.

---
+++ IC Dialog Application1/Form1.Designer.vb
@@ -0,0 +1,134 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.cmdLive = New System.Windows.Forms.Button()
+        Me.cmdProperties = New System.Windows.Forms.Button()
+        Me.cmdDevice = New System.Windows.Forms.Button()
+        Me.IcImagingControl1 = New TIS.Imaging.ICImagingControl()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        CType(Me.IcImagingControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'cmdLive
+        '
+        Me.cmdLive.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmdLive.Location = New System.Drawing.Point(666, 70)
+        Me.cmdLive.Name = "cmdLive"
+        Me.cmdLive.Size = New System.Drawing.Size(83, 23)
+        Me.cmdLive.TabIndex = 2
+        Me.cmdLive.Text = "Start Live"
+        Me.cmdLive.UseVisualStyleBackColor = True
+        '
+        'cmdProperties
+        '
+        Me.cmdProperties.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmdProperties.Location = New System.Drawing.Point(666, 41)
+        Me.cmdProperties.Name = "cmdProperties"
+        Me.cmdProperties.Size = New System.Drawing.Size(83, 23)
+        Me.cmdProperties.TabIndex = 1
+        Me.cmdProperties.Text = "Properties"
+        Me.cmdProperties.UseVisualStyleBackColor = True
+        '
+        'cmdDevice
+        '
+        Me.cmdDevice.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmdDevice.Location = New System.Drawing.Point(666, 12)
+        Me.cmdDevice.Name = "cmdDevice"
+        Me.cmdDevice.Size = New System.Drawing.Size(83, 23)
+        Me.cmdDevice.TabIndex = 0
+        Me.cmdDevice.Text = "Device"
+        Me.cmdDevice.UseVisualStyleBackColor = True
+        '
+        'IcImagingControl1
+        '
+        Me.IcImagingControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.IcImagingControl1.BackColor = System.Drawing.Color.White
+        Me.IcImagingControl1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.IcImagingControl1.DeviceListChangedExecutionMode = TIS.Imaging.EventExecutionMode.Invoke
+        Me.IcImagingControl1.DeviceLostExecutionMode = TIS.Imaging.EventExecutionMode.AsyncInvoke
+        Me.IcImagingControl1.ImageAvailableExecutionMode = TIS.Imaging.EventExecutionMode.MultiThreaded
+        Me.IcImagingControl1.LiveDisplayPosition = New System.Drawing.Point(0, 0)
+        Me.IcImagingControl1.Location = New System.Drawing.Point(10, 13)
+        Me.IcImagingControl1.Name = "IcImagingControl1"
+        Me.IcImagingControl1.Size = New System.Drawing.Size(640, 480)
+        Me.IcImagingControl1.TabIndex = 6
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(666, 151)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(83, 23)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Focus"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(666, 187)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(82, 20)
+        Me.TextBox1.TabIndex = 8
+        Me.TextBox1.Text = "350"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(669, 239)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(78, 22)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "test"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(761, 506)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.IcImagingControl1)
+        Me.Controls.Add(Me.cmdLive)
+        Me.Controls.Add(Me.cmdProperties)
+        Me.Controls.Add(Me.cmdDevice)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "IC Dialog Application1"
+        CType(Me.IcImagingControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents cmdLive As System.Windows.Forms.Button
+    Private WithEvents cmdProperties As System.Windows.Forms.Button
+    Private WithEvents cmdDevice As System.Windows.Forms.Button
+
+    Friend WithEvents IcImagingControl1 As TIS.Imaging.ICImagingControl
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ IC Dialog Application1/Form1.vb
@@ -0,0 +1,163 @@
+Imports System.Windows.Forms
+
+Public Class Form1
+
+
+    ' This attribute controls whether the live display is scaled in order
+    ' to match the size of the IC Imaging Control window on the form.
+    private m_bFitImageToWindow as boolean = True
+    Private VCDProp As TIS.Imaging.VCDHelpers.VCDSimpleProperty
+
+    Private Delegate Sub DeviceLostDelegate()
+
+    Private Delegate Sub ShowBufferDelegate(ByVal buffer As TIS.Imaging.ImageBuffer)
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ' Open the last used video capture device.
+        Try
+            IcImagingControl1.LoadDeviceStateFromFile("device.xml", True)
+        Catch
+            ' Either the xml file does not exist or the device
+            ' could not be loaded. In both cases we do nothing and proceed.
+        End Try
+
+        cmdLive.Enabled = IcImagingControl1.DeviceValid
+        cmdProperties.Enabled = IcImagingControl1.DeviceValid
+
+        if m_bFitImageToWindow = true then
+            ' Adjust live display size to the control size.
+            icImagingControl1.LiveDisplayDefault = false
+            icImagingControl1.LiveDisplayHeight = icImagingControl1.Height
+            icImagingControl1.LiveDisplayWidth = icImagingControl1.Width
+        End If
+
+
+
+    End Sub
+
+    Private Sub Form1_Closing(ByVal sender As Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles MyBase.Closing
+        If IcImagingControl1.DeviceValid = True Then
+            IcImagingControl1.LiveStop()
+        End If
+    End Sub
+
+    '
+    ' Adjust live display size to the control size.
+    '
+    Private Sub Form1_SizeChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.SizeChanged
+        If m_bFitImageToWindow = True Then
+            If IcImagingControl1.DeviceValid = True Then
+                IcImagingControl1.LiveDisplayHeight = IcImagingControl1.Height
+                IcImagingControl1.LiveDisplayWidth = IcImagingControl1.Width
+            End If
+        End If
+    End Sub
+
+    '
+    ' Show the device selection dialog of IC Imaging Control.
+    '
+    Private Sub OpenVideoCaptureDevice()
+        If IcImagingControl1.DeviceValid Then
+            StopLiveVideo()
+        Else
+            IcImagingControl1.Device = ""
+        End If
+        IcImagingControl1.ShowDeviceSettingsDialog()
+        If IcImagingControl1.DeviceValid Then
+            ' Save the currently used device into a file in order to 
+            ' be able to open it automatically at the next program start.
+            IcImagingControl1.SaveDeviceStateToFile("device.xml")
+        End If
+        cmdLive.Enabled = IcImagingControl1.DeviceValid
+        cmdProperties.Enabled = IcImagingControl1.DeviceValid
+    End Sub
+
+    '
+    ' Show the device's property dialog for modifying exposure, brightness etc. 
+    ' All changes are saved to the file 'device.xml'.
+    '
+    Private Sub ShowDeviceProperties()
+        If IcImagingControl1.DeviceValid Then
+            IcImagingControl1.ShowPropertyDialog()
+            IcImagingControl1.SaveDeviceStateToFile("device.xml")
+        End If
+    End Sub
+
+    '
+    ' Start the live video and update the state of the start/stop button.
+    '
+    Private Sub StartLiveVideo()
+        If IcImagingControl1.DeviceValid Then
+            IcImagingControl1.LiveStart()
+            cmdLive.Text = "Stop Live"
+        End If
+    End Sub
+
+    '
+    ' Stop the live video and update the state of the start/stop button.
+    '
+    Private Sub StopLiveVideo()
+        If IcImagingControl1.DeviceValid Then
+            IcImagingControl1.LiveStop()
+            cmdLive.Text = "Start Live"
+        End If
+    End Sub
+
+    '
+    ' Show the device selection dialog of IC Imaging Control.
+    '
+    Private Sub cmdDevice_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdDevice.Click
+        OpenVideoCaptureDevice()
+    End Sub
+
+    '
+    ' Show the device's property dialog for exposure, brightness etc.
+    '
+    Private Sub cmdProperties_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdProperties.Click
+        ShowDeviceProperties()
+    End Sub
+
+    ' 
+    ' Start/stop the live video and update the state of the start/stop button.
+    '
+    Private Sub cmdLive_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdLive.Click
+        If IcImagingControl1.LiveVideoRunning Then
+            StopLiveVideo()
+        Else
+            StartLiveVideo()
+        End If
+    End Sub
+
+    '
+    ' Handle the DeviceLost event.
+    '
+    Private Sub DeviceLost()
+        MessageBox.Show("Device Lost!")
+        cmdLive.Text = "Start Live"
+        cmdLive.Enabled = False
+        cmdProperties.Enabled = False
+    End Sub
+
+    Private Sub IcImagingControl1_DeviceLost(ByVal sender As System.Object, ByVal e As TIS.Imaging.ICImagingControl.DeviceLostEventArgs) Handles IcImagingControl1.DeviceLost
+        BeginInvoke(New DeviceLostDelegate(AddressOf DeviceLost))
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        VCDProp = TIS.Imaging.VCDHelpers.VCDSimpleModule.GetSimplePropertyContainer(IcImagingControl1.VCDPropertyItems)
+        VCDProp.RangeValue(TIS.Imaging.VCDIDs.VCDID_Focus) = TextBox1.Text
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim focus As Integer
+        VCDProp = TIS.Imaging.VCDHelpers.VCDSimpleModule.GetSimplePropertyContainer(IcImagingControl1.VCDPropertyItems)
+        For focus = 140 To 780 Step 20
+            VCDProp.RangeValue(TIS.Imaging.VCDIDs.VCDID_Focus) = focus
+            TextBox1.Text = focus
+            TextBox1.Update()
+            IcImagingControl1.MemorySnapImage(2000)
+            IcImagingControl1.MemorySaveImage(focus.ToString + ".bmp")
+        Next
+    End Sub
+End Class
